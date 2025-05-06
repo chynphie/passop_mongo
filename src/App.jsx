635e5519031dc55ec035b051772514475f9c82ed
chynphie <146787913+chynphie@users.noreply.github.com>
2025-05-06 16:03:33 -0400

    added background color for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ function App() {
   const [route, setRoute] = useState(window.location.pathname);
   return (
     <>
+      <div className="fixed inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+
       <Navbar />
       <BrowserRouter>
         <Routes>
